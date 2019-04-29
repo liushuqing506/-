3d29eea19cf69b72850ca88676db8f171b059c4a
BioPy <32661587+liushuqing506@users.noreply.github.com>
2019-04-29 11:27:01 +0800

    Update 001.py

--- python_爬虫/001.py
+++ python_爬虫/001.py
@@ -9,10 +9,10 @@ def getHtml(url):
 def content(html):
     # 内容分割的标签
     str1 = '<article class="article-content">' # <article class="article-content"> 之后的部分
-    content = str(html).partition(str1)[2]
+    content = str(html).partition(str1)[2] #[1]是str1的内容
 
     str2 = '<div class="article-social">' #<div class="article-social"> 之前的部分
-    content = content.partition(str2)[0]
+    content = content.partition(str2)[0]  #[1]是str2的内容
     return content  # 得到网页的内容
 
 def title(content, beg=0):
