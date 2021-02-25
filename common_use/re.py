04477919e4d8306136db3d1815a54d81ce69f97b
BioPy <32661587+liushuqing506@users.noreply.github.com>
2021-02-25 09:07:33 +0800

    Create re.py

---
+++ common_use/re.py
@@ -0,0 +1,5 @@
+a = 'barcode02.2021-02-03.21.40.34.accumulated_composition.txt'
+b = a.split('.accumulated_composition.txt')[0]
+print(b)
+print(re.match("barcode[0-9]*\.([0-9]{4}.*)",b).group(1))
+>>>2021-02-03.21.40.34
